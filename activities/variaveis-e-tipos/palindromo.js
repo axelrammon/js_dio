29bad0e83cf5c79c3b84a6be0b9c7a7e02784af8
axelrammon <axelrammon48@gmail.com>
2022-05-21 15:53:31 -0300

    add activies and rearranging repo

---
+++ activities/variaveis-e-tipos/palindromo.js
@@ -0,0 +1,22 @@
+// Solução 1
+function verificarPalindromo(string) {
+  if (!string) return "String inexistente";
+
+  return string.split("").reverse().join("") === string;
+}
+
+// Solução 2
+function verificarPalindromo2(string) {
+  if (!string) return "String inexistente";
+
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string[i] !== string[string.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true
+}
+
+console.log("Result solution 1: " + verificarPalindromo("fsd"));
+console.log("Result solution 2: " + verificarPalindromo2("omo"));
