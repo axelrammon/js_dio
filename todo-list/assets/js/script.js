ad29fc4d958bcdb6c11f5f1b351a4023f60df1db
axelrammon <axelrammon48@gmail.com>
2022-05-21 14:20:14 -0300

    adding project

--- todo-list/assets/js/script.js
+++ todo-list/assets/js/script.js
@@ -4,21 +4,31 @@ let text = document.querySelector('#text');
 
 btnAdd.addEventListener('click', () => {
 
-  let li = document.createElement('li');
-  let caixa = document.createElement('input');
-  let labelCaixa = document.createElement('label');
-  let auxiliar = text.value
+  if (text.value != '') {
+    let div_tarefas = document.createElement('div');
+    let li = document.createElement('li');
+    let caixa = document.createElement('input');
+    let labelCaixa = document.createElement('label');
+    let auxiliar = text.value;
+  
+    // labelCaixa.for = 'caixa'
+    labelCaixa.textContent = auxiliar;
+    caixa.type = 'checkbox';
+    caixa.name = 'caixa';
+    div_tarefas.className = 'div_tarefas';
+    caixa.className = 'caixa';
+    labelCaixa.className = 'label_caixa';
+  
+    ul.appendChild(div_tarefas);
+    div_tarefas.appendChild(li);
+    li.appendChild(caixa);
+    li.appendChild(labelCaixa);
+  
+    text.value = '';
 
-  // labelCaixa.for = 'caixa'
-  labelCaixa.textContent = auxiliar
-  caixa.type = 'checkbox'
-  caixa.name = 'caixa'
-  caixa.id = 'caixa'
-
-  ul.appendChild(li)
-  li.appendChild(caixa)
-  li.appendChild(labelCaixa)
-
-  text.value = ''
-
+  }
+  
 });
+
+
+
