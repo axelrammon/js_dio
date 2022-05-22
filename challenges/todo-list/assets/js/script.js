a52a63eb6acb4e886a75303dc6214ed2cf3218a7
axelrammon <axelrammon48@gmail.com>
2022-05-22 11:16:49 -0300

    update todo list and add colecoes in js activities

--- challenges/todo-list/assets/js/script.js
+++ challenges/todo-list/assets/js/script.js
@@ -2,33 +2,56 @@ let btnAdd = document.querySelector('#adicionar');
 let ul = document.querySelector('#ul_lista');
 let text = document.querySelector('#text');
 
-btnAdd.addEventListener('click', () => {
+var todos = JSON.parse(localStorage.getItem('tarefas_lista')) || [];
 
-  if (text.value != '') {
-    let div_tarefas = document.createElement('div');
-    let li = document.createElement('li');
-    let caixa = document.createElement('input');
-    let labelCaixa = document.createElement('label');
-    let auxiliar = text.value;
-  
-    // labelCaixa.for = 'caixa'
-    labelCaixa.textContent = auxiliar;
-    caixa.type = 'checkbox';
-    caixa.name = 'caixa';
-    div_tarefas.className = 'div_tarefas';
-    caixa.className = 'caixa';
-    labelCaixa.className = 'label_caixa';
-  
-    ul.appendChild(div_tarefas);
-    div_tarefas.appendChild(li);
-    li.appendChild(caixa);
-    li.appendChild(labelCaixa);
-  
-    text.value = '';
+function listarTodos() {
+  ul.innerHTML = '';
 
+  for (todo of todos) {
+    let divTodo = document.createElement('div');
+    divTodo.setAttribute('class', 'tarefa');
+    let novaTodo = document.createElement('li');
+    let textoTarefa = document.createTextNode(todo);
+    console.log(textoTarefa)
+
+    let buttonDelete = document.createElement('button');
+    buttonDelete.className = 'btnDelete';
+    let nomeDelete = document.createTextNode('Excluir');
+    buttonDelete.appendChild(nomeDelete);
+
+    let pos = todos.indexOf(todos);
+    buttonDelete.setAttribute('onclick', 'deletarTarefa('+ pos +')');
+
+    novaTodo.appendChild(textoTarefa);
+    novaTodo.appendChild(buttonDelete);
+
+    if (textoTarefa.length > 0) {
+      divTodo.appendChild(novaTodo);
+      ul.appendChild(divTodo);
+    }
+   
   }
   
-});
+}
 
+listarTodos();
 
+btnAdd.onclick = () => {
+  var novaTodo = text.value;
+
+  todos.push(novaTodo);
+  text.value = '';
+  listarTodos();
+  saveToStorage();
+}
+
+function deletarTarefa(pos) {
+  todos.splice(pos, 1);
+  listarTodos();
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('tarefas_lista', JSON.stringify(todos));
+}
 
