8075027ca7765eb300398e3acf1653568b7fda51
axelrammon <axelrammon48@gmail.com>
2022-05-28 10:39:12 -0300

    add modulos

---
+++ activities/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade("Axel", 25));
+console.log(mostraCidade("Axel", "Parazinho"));
+console.log(mostraHobby("Axel", "Guitarrar"));
